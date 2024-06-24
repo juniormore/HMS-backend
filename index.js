02bc6b65bdedeeb8be29af0f855c9e48a8feeeaf
Your Name <you@example.com>
2024-06-24 11:10:49 +0200

    fix

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-require('dotenv').config();
-
-const app = express();
-
-const port = process.env.PORT;
-app.listen(port, () => {console.log(`Server is running on port ${port}`)});
-
