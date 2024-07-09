1e3c551fa51bb8c5460f466a0549407bec104c69
Your Name <you@example.com>
2024-07-09 16:17:16 +0200

    feat: booking endpoint

--- booking/index.js
+++ booking/index.js
@@ -12,6 +12,7 @@ app.get('/test', (req, res) => {
     res.send('Hello World');
 })
 
+// api endpoint to search for a hotel room based on search criteria
 app.get('/hotelSearch', async (req, res) => {
 
     const { location, checkInDate, checkOutDate, budget, numberOfGuests } = req.query;
@@ -47,6 +48,7 @@ app.get('/hotelSearch', async (req, res) => {
     }
 });
 
+// api endpoint to retrieve hotel room information of all available rooms
 app.get('/hotelCatalog', async (req, res) => {
 
   try {
@@ -77,6 +79,7 @@ app.get('/hotelCatalog', async (req, res) => {
 
 });
 
+// api endpoint to retrieve information about a specific room using its room id
 app.get('/roomInformation/:room_id', async (req, res) => {
 	const { room_id }  = req.params;
 
@@ -107,6 +110,37 @@ app.get('/roomInformation/:room_id', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+// api endpoint to create a booking
+app.post('/booking', async (req, res) => {
+	const { booking_date, check_in_date, check_out_date, status, room_id, guest_id } = req.body;
+
+	try{
+		const result = await db.query(`
+			INSERT INTO public."Bookings"(
+				"booking_Date", 
+				"checkIn_date", 
+				"checkOut_date", 
+				"Status", 
+				"Room_ID", 
+				"Guest_ID")
+			VALUES (
+				'${booking_date}', 
+				'${check_in_date}', 
+				'${check_out_date}', 
+				'${status}', 
+				${room_id}, 
+				${guest_id});
+			`);
+
+		res.status(201).send('Booking created successfully');
+		console.log(result.rows);
+
+	}catch(err){
+		console.error(err);
+		res.status(500).send('Internal Server Error');
+	}
+}); 
 
 const port = process.env.PORT;
 app.listen(port, () => {console.log(`Server is running on port ${port}`)});
