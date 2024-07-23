62a487de39fa105363bc87ea8eaa9a8072aa2ca3
Junior Moremong <jmoremong@hotmail.com>
2024-07-23 14:53:23 +0200

    added recaptcha verification endpoint

--- feedback/server.js
+++ feedback/server.js
@@ -3,6 +3,9 @@ const db = require('./db');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const axios = require('axios');
+
+const SITE_SECRET = process.env.SITE_SECRET;
 
 app.use(express.json());
 app.use(cors());
@@ -15,38 +18,29 @@ app.get('/', (req, res) => {
     res.send('This API collects feedback from the front-end and sends it to the database.');
 });
 
+app.post('/api/verify', async (req, res) => {
+	const { captchaValue } = req.body
+	const { data } = await axios.post(
+		`https://www.google.com/recaptcha/api/siteverify?secret=${SITE_SECRET}&response=${captchaValue}`,
+	)
+	res.send(data)
+})
+
 app.post('/api/feedback',  async (req, res) => {
-    const { name, surname, email, discovery_source, improvements, rating, Suggestions } = req.body;
-    
+    const { name, surname, email, discovery_source, improvements, rating, suggestions } = req.body;
 	try{
-		const result = await db.query(`
-			INSERT INTO public."Feedback"(
-				"Hotel_ID", 
-				"Name", 
-				"Surname", 
-				"Email", 
-				"discovery_source", 
-				"Improvements",
-				"rating",
-				"Suggestions")
-			VALUES (
-                '1',
-				'${name}', 
-				'${surname}', 
-				'${email}', 
-				'${discovery_source}', 
-                '${improvements}',
-				${rating}, 
-				'${Suggestions}');
-			`);
 
-		res.status(201).send('Feedback submitted successfully.');
-		console.log(result.rows);
-
-	}catch(err){
-		console.error(err);
-		res.status(500).send('Internal Server Error');
-	}
+		const query = 'INSERT INTO "Feedback" ("Hotel_ID", "Name", "Surname", "Email", discovery_source, "Improvements", rating, "Suggestions") VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
+    	const values = [1, name, surname, email, discovery_source, improvements, rating, suggestions];
+    
+    
+    const result = await db.query(query, values);
+    
+    res.status(201).json({ message: 'Feedback received successfully!', feedback: result.rows[0] });
+  } catch (error) {
+    console.error('Error saving feedback:', error);
+    res.status(500).json({ error: 'An error occurred while saving feedback.' });
+  }
 })
 
 const port = process.env.PORT;
